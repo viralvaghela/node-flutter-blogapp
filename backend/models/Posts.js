86f1955581e207d3e4ff98d517f8e1790fc8fb39
viralvaghela <viralvaghela7109@gmail.com>
2020-09-12 13:14:05 +0530

    POST CRUD Donegit add .!

--- backend/models/Posts.js
+++ backend/models/Posts.js
@@ -4,18 +4,18 @@ const postScheme = new mongooes.Schema({
     title:{
         type:String,
         min:5,
-        required:true
+        
         
     },
     body:{
         type:String,
         min:5,
         max:5000,
-        required:true
+       
     },
     date:{
         type:Date,
-        required:true
+        
     }
 });
 
--- backend/posts/post.js
+++ backend/posts/post.js
@@ -1,8 +1,56 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Post = require('../models/Posts');
+const Post = require("../models/Posts");
 
-router.get("/",(req,res)=>{
-    res.send("Welcome to post");
+//Get All posts
+router.get("/", async (req, res) => {
+  try {
+    const post = await Post.find();
+    res.json(post);
+  } catch (err) {
+    res.json(err);
+  }
 });
-module.exports=router;
+
+///Delete single post
+router.delete("/:postId", async (req, res) => {
+  try {
+    const deletedPost = await Post.remove({
+      _id: req.params.postId,
+    });
+    res.json(deletedPost);
+  }
+  catch (err) {
+    res.json(err);
+  }
+});
+
+//Edit post we will edit title and body both
+router.patch("/:postId", async (req, res) => {
+  try {
+    const updatedPost = await Post.updateOne(
+      { _id: req.params.postId },
+      { $set: { title: req.body.title, body: req.body.body } }
+    );
+    res.json(updatedPost);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+//Submit post
+router.post("/", async (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    body: req.body.body,
+    date: Date.now(),
+  });
+  try {
+    const addedUser = await post.save();
+    res.json(addedUser);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+module.exports = router;
